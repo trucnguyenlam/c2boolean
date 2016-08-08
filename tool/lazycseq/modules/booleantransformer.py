2e27c3eaab990f9d194a1df86e718b941e5f7a79
Truc Nguyen Lam <trucnguyenlam@gmail.com>
2016-08-08 11:52:47 +0100

    add module booleantransformer template

---
+++ tool/lazycseq/modules/booleantransformer.py
@@ -0,0 +1,47 @@
+""" Lazy-CSeq boolean transformer module
+"""
+VERSION = '0.1'
+
+"""
+
+Transformation:
+
+
+
+Prerequisites:
+	- all functions should have been inlined, except:
+		+ the main(),
+		+ all thread functions,
+		+ all __CSEQ_atomic_ functions,
+		+ and function __CSEQ_assert
+	- all loops should habe been unrolled
+	- no two threads refers to the same thread function (use module duplicator.py)
+
+TODO:
+	-
+
+Changelog:
+	2016.08.08  Intial version
+
+"""
+import math, re
+from time import gmtime, strftime
+import pycparser.c_parser, pycparser.c_ast, pycparser.c_generator
+import core.common, core.module, core.parser, core.utils
+
+
+class booleantransformer(core.module.Translator):
+	def init(self):
+		''' Initialization of a module, please refer to core.module.Translator
+			Use two function to set (optional) input or output of this module
+		'''
+		# self.addInputParam(self,id,description,datatype,default,optional)
+		# self.addOutputParam(self,id,description='',datatype='')
+
+
+	def loadfromstring(self, string, env):
+		'''	Load string from the previous module (string) and perform translation
+			please refer to core.module.Translator loadfromstring for more details
+		'''
+		super(self.__class__, self).loadfromstring(string, env)
+
